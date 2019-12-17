e4e7ccca4851f1725bbbea9400dc30d556715fd9
Guido Beijk <g.beijk@student.utwente.nl>
2019-12-17 14:21:10 +0100

    Initial Commit

---
+++ mEAT-Up/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  mEAT-Up
+//
+//  Created by Guido Beijk on 17/12/2019.
+//  Copyright © 2019 Guido Beijk. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
